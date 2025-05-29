bdba6506b94bafa5c4dde57770571c47425c309e
huguinz <hugolopessouza58@gmail.com>
2025-05-29 01:39:51 -0300

    feat: update registerNewUser function to include dateOfBirth input and improve request structure

--- src/scripts/modules/api/loginPage/post.js
+++ src/scripts/modules/api/loginPage/post.js
@@ -3,8 +3,26 @@
 import { loginPageElements } from '../../elements/loginPage/elements.js'
 
 export async function registerNewUser() {
-	const { nameUserRegister, emailUserRegister, passwordUserRegister, telephoneUserRegister } = loginPageElements()
+	const { nameUserRegister, emailUserRegister, passwordUserRegister, dateOfBirthInput } = loginPageElements()
 
 	const url = ''
-	const response = await fetch()
+
+	const body = {
+		nome: nameUserRegister,
+		email: emailUserRegister,
+		endereco: '',
+		cnpj: '',
+		password: passwordUserRegister,
+		data_nascimento: dateOfBirthInput
+	}
+
+	const options = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(body)
+	}
+
+	const response = await fetch(url, options)
 }
